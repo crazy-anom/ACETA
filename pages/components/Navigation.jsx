4e045cb4eb600ec024273a2d691ae9597084d8a7
Gowdaman p <56969439+crazy-anom@users.noreply.github.com>
2022-10-17 10:30:55 -0700

    Update Navigation.jsx

--- pages/components/Navigation.jsx
+++ pages/components/Navigation.jsx
@@ -5,7 +5,7 @@ const Navigation = ()=>{
             <>
                 <nav className = "flex justify-between px-5 py-5 items-center">
                     <h1 className = "font-bold text-2xl"><span className = "text-red-400">A</span>CETA</h1>
-                    <ol className = "flex justify-between">
+                    <ol className = "flex justify-between text-sm">
                         <li className = "ml-5 hover:text-red-400"><a href="./">Home</a></li>
                         <li className = "ml-5 hover:text-red-400"><a href="./Learn">Learn</a></li>
                         <li className = "ml-5 hover:text-red-400"><a href="./Software">Software</a></li>
